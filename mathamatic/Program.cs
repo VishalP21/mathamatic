24b6da08bb9879ce0a8ed62a82b92420c3df3e9a
Vishal Prajapati <vishalp951295@gmail.com>
2024-08-14 11:57:01 +0530

    Add project files.

---
+++ mathamatic/Program.cs
@@ -0,0 +1,8 @@
+﻿using mathamatic;
+
+maths maths = new maths();
+
+maths.add(25, 10);
+maths.mins(25, 10,10);
+maths.multis(25, 10,8);
+maths.divi(25, 10);
---
+++ mathamatic/maths.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mathamatic
+{
+    public class maths
+    {
+        
+        public void add(int x, int y)
+        {
+            Console.WriteLine(x + y);
+           
+        }
+        public void add(int x, int y, int z) 
+        {
+            Console.WriteLine(x + y + z);
+
+        }
+        public void mins(int x, int y)
+        {
+            Console.WriteLine(x - y);
+           
+        }
+        public void mins(int x, int y, int z)
+        {
+            Console.WriteLine(x - y - z);
+           
+        }
+        public void multis(int x, int y)
+        {
+           Console.WriteLine(x * y);
+        }
+        public void multis(int x, int y, int z)
+        {
+            Console.WriteLine(x * y * z);
+        }
+        public void divi(int x, int y)
+        {
+            Console.WriteLine(x / y);
+        }
+        public void divi(int x, int y, int z)
+        {
+            Console.WriteLine(x / y / z);
+        }
+    }
+}
